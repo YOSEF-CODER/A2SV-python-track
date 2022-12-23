20e862a8ac6d09fdd9111e64115779ad846f3e52
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-12-23 19:17:32 +0300

    Create NestedList.py

---
+++ NestedList.py
@@ -0,0 +1,10 @@
+list=[]
+if __name__ == '__main__':
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        record=[name,score]
+        list.append(record)
+        
+
+        
