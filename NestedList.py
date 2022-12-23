90d0bf93841ab9b3448b619d90e3fdb80ac35c01
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-12-23 20:04:47 +0300

    Update NestedList.py

--- NestedList.py
+++ NestedList.py
@@ -1,10 +1,33 @@
-list=[]
+score_collection=[]
+only_sc=[]
+names=[]
 if __name__ == '__main__':
     for _ in range(int(input())):
         name = input()
         score = float(input())
         record=[name,score]
-        list.append(record)
+        score_collection.append(record)
+# print(score_collection)
         
+for x in score_collection:
+    only_sc.append(x[1])
 
-        
+only_sc=sorted(only_sc)
+
+only_sc=list(dict.fromkeys(only_sc))
+
+# print(only_sc)
+
+second_score=only_sc[1]
+
+# print(second_score)
+
+for x in range(len(score_collection)):
+    if second_score == score_collection[x][1]:
+        names.append(score_collection[x][0])
+
+names=sorted(names)
+
+for name in names:
+    print(name)
+
