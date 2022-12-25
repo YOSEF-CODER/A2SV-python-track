417779df98871f92be2e89368f19773f3cae281c
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-12-25 22:49:55 +0300

    Time: 32 ms (94.96%), Space: 13.8 MB (63.06%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -4,12 +4,6 @@ class Solution:
         for x in nums:
             if int(x) == val:
                 output+=1
-                # nums.remove(val)
-        print(output)
         for x in range(output):
             nums.remove(val)
-            print(nums)
-        # for x in range(output):
-        #     nums.append('_')
-        print(output)
         return len(nums)
