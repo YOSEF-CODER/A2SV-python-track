eff0990cad94079f575d2761fd42c86816872a74
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-12-26 01:27:45 +0300

    Time: 283 ms (5.03%), Space: 14.3 MB (15.45%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        left=0
+        right=len(arr)-1
+        arr=sorted(arr)
+        print(arr)
+        while left<right:
+            if arr[left] * 2 == arr[right] or arr[left] == arr[right] * 2:
+                return True
+            else:
+                if left == right-1:
+                    left=0
+                    right-=1
+                else:
+                    print(left,right)    
+                    left+=1
+                    
+        return False
