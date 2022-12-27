321b6515232fba1220c7526dc4f99976cb3c471e
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-12-27 08:04:55 +0300

    Create Set-union.py

---
+++ Set-union.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOU
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+if __name__ == '__main__':
+    n_eng=input()
+    eng_stu=set(list(map(int, input().split())))
+    n_french=input()
+    french_stu=set(list(map(int, input().split())))
+    diff=eng_stu.union(french_stu)
+    print(len(diff))
+    
