766548db4f1f9f997d3f9c41fee9255412f194be
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2022-12-27 19:53:38 +0300

    Create No-Idea.py

---
+++ No-Idea.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n, m = input().split()
+sc_ar = input().split()
+
+A = set(input().split())
+B=  set(input().split())
+
+C=sum([(i in A) - (i in B) for i in sc_ar])
+
+print(C)
