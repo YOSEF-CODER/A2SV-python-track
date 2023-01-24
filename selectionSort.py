c40daa26e2d926ea489042937da8eb9051256e97
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-01-24 15:25:56 +0300

    Create selectionSort.py

---
+++ selectionSort.py
@@ -0,0 +1,25 @@
+#User function Template for python3
+
+class Solution: 
+    def selectionSort(self, arr,n):
+        for i in range(n):
+            minIndex = i
+            for j in range(i,n):
+                if(arr[minIndex] > arr[j]):
+                    minIndex = j
+            arr[minIndex],arr[i]=arr[i],arr[minIndex]
+        return arr
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
