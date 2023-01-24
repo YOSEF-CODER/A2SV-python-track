11f35a79627620fa4cf8478de11473aed4711b89
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-01-25 02:18:31 +0300

    Time: 39 ms (74.98%), Space: 13.9 MB (32.66%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,7 +3,19 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        nums1[m:]=nums2
-        nums1.sort()
-        print(nums1)
+        left_pointer = m - 1
+        right_pointer = n - 1
+        zero_pointer = m + n - 1
         
+        while right_pointer >= 0:
+            if left_pointer >= 0:
+                if nums1[left_pointer] > nums2[right_pointer]:
+                    nums1[zero_pointer] = nums1[left_pointer]
+                    left_pointer -= 1
+                else:
+                    nums1[zero_pointer] = nums2[right_pointer]
+                    right_pointer -= 1
+                zero_pointer -= 1
+            else:
+                nums1[right_pointer] = nums2[right_pointer]
+                right_pointer -=1
