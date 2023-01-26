eb797c6dc193c4e8647ce94893a2ffe1f13c3ddc
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-01-26 18:06:33 +0300

    Time: 43 ms (71.04%), Space: 13.8 MB (61.16%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,21 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        lastOfIndex={}
+        answer = []
+        for i in range(len(s)):
+            lastOfIndex[s[i]]=i
+         
+        left = 0
+        right = 0
+        while left < len(s):
+            temp = lastOfIndex[s[left]]
+            while right < temp:
+                lastIndex = lastOfIndex[s[right]]
+                if lastIndex > temp:
+                    temp = lastIndex
+                right += 1
+            answer.append((right - left) + 1)
+            right += 1
+            left = right
+            
+        return answer
