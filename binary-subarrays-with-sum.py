0a0eedd555899e06a0eeae68ce9315e393c231c1
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-03-06 10:34:50 +0300

    added binary-subarrays-with-sum.python

---
+++ binary-subarrays-with-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        count=0
+        curSum=0
+        preSum={0:1}
+        for n in nums:
+            curSum+=n
+            diff=curSum-goal
+            
+            count+=preSum.get(diff,0)
+            print(preSum.get(diff,0))
+            preSum[curSum]=1+preSum.get(curSum,0)
+        
+       
+        return count
