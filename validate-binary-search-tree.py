90f71f0ddeea33444b0390ba6b35ee748acdc3f7
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-03-07 23:24:37 +0300

    added validate-binary-search-tree.python

--- validate-binary-search-tree.py
+++ validate-binary-search-tree.py
@@ -6,15 +6,22 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        arr=[]
-        def traverse(root):
-           if not root:
-               return
-           traverse(root.left)
-           arr.append(root.val)
-           traverse(root.right)
-        traverse(root)
-        for i in range(len(arr)-1):
-            if arr[i]>=arr[i+1]:
-                return False
-        return True
+        def helper(root, left, right):
+            if not root:
+                return True
+
+            l = False
+            r = False
+            if left < root.val < right:
+                l = helper(root.left, left, root.val)
+            else:
+                l = False
+            
+            if left < root.val < right:
+                r = helper(root.right, root.val, right)
+            else:
+                r = False
+
+            return l and r
+
+        return helper(root, float('-inf'), float('inf'))
