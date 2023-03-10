f842991decb9dc39744768230a7463b096757972
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-03-10 11:54:39 +0300

    added grid-game.python

--- grid-game.py
+++ grid-game.py
@@ -1,24 +1,12 @@
 class Solution:
+
     def gridGame(self, grid: List[List[int]]) -> int:
-        N=len(grid[0])
-        preRow1,preRow2=grid[0].copy(),grid[1].copy()
+        top = sum(grid[0])
+        bottom= 0
+        answer = float('inf')
 
-
-        for i in range(1,N):
-            preRow1[i]+=preRow1[i-1]
-            preRow2[i]+=preRow2[i-1]
-
-        res=float('inf')
-
-        for i in range(N):
-            top=preRow1[-1]-preRow1[i]
-            bottom=preRow2[i-1] if i>0 else 0
-            secondRobot=max(top,bottom)
-            res=min(res,secondRobot)
-
-
-
-
-
-
-        return res
+        for grid0, grid1 in zip(grid[0], grid[1]):
+            top -= grid0
+            answer = min(answer, max(top, bottom))
+            bottom += grid1
+        return answer
