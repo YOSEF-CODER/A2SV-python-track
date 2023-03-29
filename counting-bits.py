77f62b4bc3b1ccfbefd6a576ba151296f3b79b06
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-03-29 18:43:36 +0300

    added counting-bits.python

--- counting-bits.py
+++ counting-bits.py
@@ -1,9 +1,8 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        n=n+1
-        ans=[0]*n
+        ans=[0] * (n+1)
 
-        for i in range(n):
-            ans[i]=list(bin(i)).count('1')
-
+        for i in range(1,n+1):
+            ans[i]=ans[i>>1] + (i&1)
+    
         return ans
