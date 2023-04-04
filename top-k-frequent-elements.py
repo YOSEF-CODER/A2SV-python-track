22184d3eb6a75301a78b04fc6084af683e2fcabc
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-04 19:15:51 +0300

    added top-k-frequent-elements.python

--- top-k-frequent-elements.py
+++ top-k-frequent-elements.py
@@ -24,25 +24,3 @@ class Solution:
         quickSort(0, len(nums)-1)
         
         return nums[:k]
-        
-        def quickSort(start, end):
-            
-            if start >= end:
-                return
-
-            ptr = start + 1
-            for i in range(start+1, end+1):
-                
-                if count[nums[i]] > count[nums[start]]:
-                    
-                    nums[i], nums[ptr] = nums[ptr], nums[i]
-                    ptr += 1
-
-            nums[start], nums[ptr-1] = nums[ptr-1], nums[start]
-
-            quickSort(start, ptr-2)
-            quickSort(ptr, end)
-
-        quickSort(0, len(nums)-1)
-        
-        return nums[:k]
