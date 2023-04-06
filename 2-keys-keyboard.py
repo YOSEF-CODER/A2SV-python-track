f4fe5e881c8d6797d1bffff053952bd6d75f1891
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-06 17:44:38 +0300

    added 2-keys-keyboard.python

--- 2-keys-keyboard.py
+++ 2-keys-keyboard.py
@@ -1,12 +1,13 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        result=0
+        res=0
         d=2
+
 
         while n>1:
             while n%d==0:
-                result+=d
-                n//=d 
+                res+=d
+                n//=d
             d+=1
-        
-        return result
+
+        return res
