9119cced90c7f5788bd97c1ef9a7ad8cf89ec736
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-06 18:14:02 +0300

    added x-of-a-kind-in-a-deck-of-cards.python

--- x-of-a-kind-in-a-deck-of-cards.py
+++ x-of-a-kind-in-a-deck-of-cards.py
@@ -1,16 +1,16 @@
 class Solution:
     def hasGroupsSizeX(self, deck: List[int]) -> bool:
-        def gcd(a,b):
-            if b==0:
-                return a
-            return gcd(b,a%b)
-        arr=list(Counter(deck).values())
-
-        if min(arr)<=1:
+        if len(deck)==1:
             return False
 
-        for i in range(1,len(arr)):
-            if gcd(arr[i-1],arr[i])<=1:
-                return False
+        arr=list(Counter(deck).values())
+
+        partition=gcd(*arr)
+
+        if partition == 1:
+            return False
+
+
+
 
         return True
