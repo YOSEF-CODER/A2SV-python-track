cd44d49e2b766ae6e6fdf00b05f08a75c5b7b4f4
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-06 18:29:09 +0300

    added find-greatest-common-divisor-of-array.python

--- find-greatest-common-divisor-of-array.py
+++ find-greatest-common-divisor-of-array.py
@@ -1,9 +1,3 @@
 class Solution:
     def findGCD(self, nums: List[int]) -> int:
-        a=min(nums)
-        b=max(nums)
-        def gcd(a, b):
-            if b == 0:
-                return a
-            return gcd(b, a % b)
-        return gcd(a,b)
+        return gcd(min(nums),max(nums))
