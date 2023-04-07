1c037df0e71bc312000ef57f5192324b44924955
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-07 11:15:50 +0300

    Create From adjacency matrix to adjacency list.py

---
+++ From adjacency matrix to adjacency list.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+n=int(input())
+arr=[]
+graph=defaultdict(list)
+
+for _ in range(n):
+    arr.append(list(map(int,input().split())))
+
+for i in range(n):
+    for j in range(n):
+        if arr[i][j] >=1:
+            graph[i+1].append(j+1)
+for key, value in graph.items():
+    print(len(value),*value)
+
