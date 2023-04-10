580d530c98070842a2e8ccbad4a99219d6154d0f
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-04-10 13:36:04 +0300

    added maximum-depth-of-n-ary-tree.python

---
+++ maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,25 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        visited=set()
+        Max=0
+
+        def dfs(root,level):
+            nonlocal Max
+            if not root:
+                return
+
+            Max=max(Max,level)
+
+            for child in root.children:
+                dfs(child,level+1)
+        dfs(root,1)
+
+        return Max
