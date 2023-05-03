5a67b7dacdb61c1a4e4d817619119e7456b7f145
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-03 18:30:36 +0300

    added kth-largest-element-in-an-array.python

--- kth-largest-element-in-an-array.py
+++ kth-largest-element-in-an-array.py
@@ -1,38 +1,3 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        N=len(nums)
-        def partition(left,right):
-            pivot=nums[right]
-
-            i=left-1
-
-            for j in range(left,right):
-                if nums[j] <= pivot:
-                    i=i+1
-                    nums[i],nums[j]=nums[j],nums[i]
-
-            nums[i+1],nums[right]=nums[right],nums[i+1]
-            # print(nums)
-            return i+1
-        
-        def quicksort(left,right):
-
-
-            if right < left:
-                return nums[N-k]
-
-            pi=partition(left,right)
-
-            if pi == N-k:
-                return nums[pi]
-            elif pi < N-k:
-                return quicksort(pi+1,right)
-            else:
-                return quicksort(left,pi-1)
-        return quicksort(0,N-1)
-
-
-
-        
-
-        return nums[-ans]
+        return  heapq.nlargest(k,nums)[-1]
