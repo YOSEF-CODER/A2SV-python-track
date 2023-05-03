5bc2012831dd28b8f2be0db7de6cf48963d8ba9b
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-03 21:35:38 +0300

    added number-of-nodes-in-the-sub-tree-with-the-same-label.python

---
+++ number-of-nodes-in-the-sub-tree-with-the-same-label.py
@@ -0,0 +1,50 @@
+class Solution:
+    def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
+        ans = [0]*n
+        graph = defaultdict(list)
+        for edge in edges:
+            graph[edge[0]].append(edge[1])
+            graph[edge[1]].append(edge[0])
+
+        print(graph)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+        
+        def dfs(idx, parent):
+            count= collections.Counter()
+            for child in graph[idx]:
+                if child!= parent:
+                    count+= dfs(child, idx)
+            count[labels[idx]] +=1
+            ans[idx] = count[labels[idx]]
+            return count
+            
+        dfs(0, -1)
+        return ans
