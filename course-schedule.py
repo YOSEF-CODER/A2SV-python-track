54f76ed8028f85553a367954b3d811335f8470ca
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-09 18:44:01 +0300

    added course-schedule.python

---
+++ course-schedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adjList=defaultdict(list)
+        orderedCourses=[]
+        indegree=[0]*numCourses
+
+        for a,b in prerequisites:
+            adjList[b].append(a)
+            indegree[a]+=1
+
+        queue=deque()
+
+        for i in range(numCourses):
+            if indegree[i]==0:
+                queue.append(i)
+
+        while queue:
+            for _ in range(len(queue)):
+                node=queue.popleft()
+                orderedCourses.append(node)
+
+                for course in adjList[node]:
+                    indegree[course]-=1
+                    if indegree[course]==0:
+                        queue.append(course)
+        for dep in indegree:
+            if dep:
+                return False
+        return True
