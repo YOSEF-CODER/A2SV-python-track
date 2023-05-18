eaf106fdde120a01ebbb5144437139d4d837881b
yosef lakew <80267669+YOSEF-CODER@users.noreply.github.com>
2023-05-18 17:50:25 +0300

    added find-if-path-exists-in-graph.python

--- find-if-path-exists-in-graph.py
+++ find-if-path-exists-in-graph.py
@@ -1,24 +1,30 @@
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        adjList=defaultdict(list)
-
-        for i,j in edges:
-            adjList[i].append(j)
-            adjList[j].append(i)
-
-        def findPath(node,visited):
-            nonlocal destination
-
-            if node==destination:
-                return True
-
-            if node in visited:
-                return False
-
-            visited[node]=True
-
-            for path in adjList[node]:
-                if findPath(path,visited):
-                    return True
-
-        return findPath(source,{})
+        parent = [i for i in range(n)]
+        size = [0]*n
+        # let's create a union find 
+        def find(member):
+            if member == parent[member]:
+                return member
+            
+            parent[member] = find(parent[member])
+            return parent[member]
+        
+        def union(x,y):
+            xpar = find(x)
+            ypar = find(y)
+        
+            if size[xpar] > size[ypar]:
+                parent[ypar] = xpar
+                size[xpar] += size[ypar]
+            else:
+                parent[xpar] = ypar
+                size[ypar] += size[xpar]
+        
+        def isConnected(x,y):
+            return find(x) == find(y)
+        
+        for x,y in edges:
+            union(x,y)
+        
+        return isConnected(source, destination)
