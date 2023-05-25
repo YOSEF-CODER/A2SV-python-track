111d36bd1ca28028fae5f5fb84f9671ed7e0d0a9
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-05-25 18:07:15 +0300

    added lemonade-change.python

--- lemonade-change.py
+++ lemonade-change.py
@@ -2,7 +2,7 @@ class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
         fives=0
         tens=0
-        twenties=0
+       
 
         for bill in bills:
             if bill==5:
@@ -20,7 +20,6 @@ class Solution:
                     fives-=1
                 elif fives>=3:
                     fives-=3
-                    twenties+=1
                 else:
                     return False
         return True
