8f77e13d7407aae8d0e7cfdb5d009ee9efd3a51f
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-05-25 18:06:48 +0300

    added lemonade-change.python

---
+++ lemonade-change.py
@@ -0,0 +1,26 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        fives=0
+        tens=0
+        twenties=0
+
+        for bill in bills:
+            if bill==5:
+                fives+=1
+            elif bill==10:
+                if fives>0:
+                    fives-=1
+                    tens+=1
+                else:
+                    return False
+            elif bill==20:
+           
+                if fives>0 and tens>0:
+                    tens-=1
+                    fives-=1
+                elif fives>=3:
+                    fives-=3
+                    twenties+=1
+                else:
+                    return False
+        return True
