65a18570942aeac548f41b537f346f148d72928f
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-05-27 21:45:41 +0300

    added increasing-triplet-subsequence.python

---
+++ increasing-triplet-subsequence.py
@@ -0,0 +1,21 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        Min=nums[0]
+        subsequence=[]
+
+
+        for num in nums[1:]:
+            if num <= Min:
+                Min=num
+            else:
+                subsequence.append(num)
+        if not subsequence:
+            return False
+        Min=subsequence[0]
+
+        for sub in subsequence[1:]:
+            if sub <= Min:
+                Min = sub
+            else:
+                return True
+        return False
