8d5fd23dfd2d4b698bd4b7bb4af361ff71b49e49
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-05-30 17:43:27 +0300

    added fibonacci-number.python

--- fibonacci-number.py
+++ fibonacci-number.py
@@ -1,16 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
         #another function written to use memoization
-        def fibonacci(n, memo={}):
+        memo={}
+        def fibonacci(n):
             if n in memo:
                 return memo[n]
             
-            if n == 1:
-                return 1
-            elif n == 0:
-                return 0
-
+            if n == 1 or n == 0:
+                return n
+         
             memo[n] = fibonacci(n-1) + fibonacci(n-2)
+
             return memo[n]
             
         return fibonacci(n)
