f412cad4dc183ac8951216b98a326cb237dd4034
yosef lakew <80267669+yoseflakew25@users.noreply.github.com>
2023-05-30 17:44:19 +0300

    added fibonacci-number.python

--- fibonacci-number.py
+++ fibonacci-number.py
@@ -3,13 +3,12 @@ class Solution:
         #another function written to use memoization
         memo={}
         def fibonacci(n):
-            if n in memo:
-                return memo[n]
+           
             
             if n == 1 or n == 0:
                 return n
-         
-            memo[n] = fibonacci(n-1) + fibonacci(n-2)
+            if n not in memo:
+                memo[n] = fibonacci(n-1) + fibonacci(n-2)
 
             return memo[n]
             
